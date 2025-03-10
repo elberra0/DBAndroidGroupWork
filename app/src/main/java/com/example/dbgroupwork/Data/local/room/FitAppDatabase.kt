2eed9f4fdcdc8a99e3349f3cec127785e46bfc9d
SCHAD\rperalta <rperalta@schad.do>
2025-03-10 08:20:39 -0400

    config db room

--- app/src/main/java/com/example/dbgroupwork/Data/local/room/FitAppDatabase.kt
+++ app/src/main/java/com/example/dbgroupwork/Data/local/room/FitAppDatabase.kt
@@ -7,6 +7,22 @@ import androidx.room.RoomDatabase
 @Database(entities = [PlanLocal::class, ClasificacionLocal::class], exportSchema = true , version = 1)
 abstract class FitAppDatabase: RoomDatabase() {
     abstract fun getPlanDao(): PlanDao
-    abstract fun getClacificacionDao(): ClasificacionDao
+    abstract fun getClasificacionDao(): ClasificacionDao
 
+    companion object {
+        var db:FitAppDatabase? = null
+
+        fun provideDatabase(application: Context): FitAppDatabase {
+            if(db == null) {
+                synchronized(this)
+                {
+                    db = Room.databaseBuilder(
+                        application,
+                        FitAppDatabase::class.java,"FitAppDatabase.db"
+                    ).build()
+                }
+            }
+            return db!!
+        }
+    }
 }

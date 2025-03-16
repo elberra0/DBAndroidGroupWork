31c2470745bc14eb8a24ace6173abfd76d0ef383
rperalta <anperalta@gmail.com>
2025-03-16 02:14:33 +0100

    add foreignKey

--- app/src/main/java/com/example/dbgroupwork/data/local/room/PlanLocal.kt
+++ app/src/main/java/com/example/dbgroupwork/data/local/room/PlanLocal.kt
@@ -1,10 +1,20 @@
 package com.example.dbgroupwork.data.local.room
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import com.example.dbgroupwork.Domain.Models.Clasificacion
 
 
-@Entity(tableName = "PlanRoom")
+@Entity(
+    tableName = "PlanRoom",
+    foreignKeys = [ForeignKey(
+        entity = ClasificacionLocal::class,
+        parentColumns = ["id"],
+        childColumns = ["clasificacionid"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 data class PlanLocal(
     @PrimaryKey val id: Int,
     val clasificacionid: Int,
